a5629168f796e07007eef5903884664ef586bd85
Shahbaz Hasan Raja <shahbaz.hasanrja@letstransport.team>
2022-08-09 18:08:32 +0530

    Added Controllers

--- dist/routes/todos.js
+++ dist/routes/todos.js
@@ -1,37 +1,10 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = require("express");
-let todos = [];
+const todos_1 = require("../controllers/todos");
 const router = (0, express_1.Router)();
-router.get('/', (req, res, next) => {
-    res.status(200).json({
-        message: 'Fechted all Tasks',
-        todos: todos
-    });
-});
-router.post('/newTodo', (req, res, next) => {
-    const body = req.body;
-    const newTodo = {
-        id: new Date().toISOString(),
-        text: body.text
-    };
-    todos.push(newTodo);
-    return res.status(201).json({ message: 'New Todo Added successfully',
-        newTodo: newTodo });
-});
-router.put('/updateTodo/:todoId', (req, res, next) => {
-    const body = req.body;
-    const tid = req.params.todoId;
-    const todoIndex = todos.findIndex(todoItem => todoItem.id === tid);
-    if (todoIndex > -1) {
-        todos[todoIndex].text = body.text;
-        return res.status(200).json({ message: 'Successfully Altered Todo Task', todos: todos });
-    }
-    return res.status(404).json({ message: 'Invalid ID for Todo Task' });
-});
-router.delete('/deleteTodo/:todoId', (req, res, next) => {
-    const tid = req.params.todoId;
-    todos = todos.filter((todoItem) => todoItem.id !== tid);
-    res.status(200).json({ message: 'Deleted Todo Task', todos: todos });
-});
+router.get('/', todos_1.getAllTodos);
+router.post('/newTodo', todos_1.createTodo);
+router.put('/updateTodo/:id', todos_1.updateTodo);
+router.delete('/deleteTodo/:id', todos_1.deleteTodo);
 exports.default = router;
--- src/routes/todos.ts
+++ src/routes/todos.ts
@@ -1,48 +1,15 @@
 import {Router} from 'express';
-import {Todo} from '../models/todo'
+import {createTodo, getAllTodos, deleteTodo , updateTodo} from '../controllers/todos'
 import { appendFile } from 'fs';
 
-
-let todos : Array<Todo>=[];
 const router = Router();
-type Requestbody = {
-  text : string
-}
 
-router.get('/', (req, res, next) => {
- res.status(200).json({
-  message : 'Fechted all Tasks',
-  todos: todos
- });
-});
+router.get('/', getAllTodos);
 
-router.post('/newTodo', (req, res, next) => {
-  const body = req.body as Requestbody;
-  const newTodo : Todo = {
-    id : new Date().toISOString(),
-    text : body.text
-  };
-  todos.push(newTodo);
-  return res.status(201).json({message : 'New Todo Added successfully' ,
-  newTodo: newTodo});
-});
+router.post('/newTodo', createTodo);
 
-router.put('/updateTodo/:todoId', (req, res, next) => {
-  const body = req.body as  Requestbody;
-  const tid = req.params.todoId;
-  const todoIndex = todos.findIndex(todoItem  => todoItem.id === tid);
-  if(todoIndex > -1)
-  {
-    todos[todoIndex].text = body.text;
-    return res.status(200).json({message : 'Successfully Altered Todo Task', todos : todos});
-  }
-  return res.status(404).json({message : 'Invalid ID for Todo Task'});
-});
+router.put('/updateTodo/:id', updateTodo);
 
-router.delete('/deleteTodo/:todoId', (req, res, next) => {
-  const tid = req.params.todoId;
-  todos = todos.filter((todoItem) => todoItem.id !== tid);
-  res.status(200).json({message : 'Deleted Todo Task', todos : todos});
-});
+router.delete('/deleteTodo/:id', deleteTodo);
 
 export default router;
